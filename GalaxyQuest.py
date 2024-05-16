bcd43eeadf1e5b8965d5b69cdb62ebedc4d8cfcc
WarriorOfZera <127200677+WarriorOfZera@users.noreply.github.com>
2024-05-16 07:41:49 +0200

    commit promjena mjesta
    Promijenio sam mijesta fajlova, kao i pokusao da inkorporiram nešto unutar igre

--- GalaxyQuest.py
+++ GalaxyQuest.py
@@ -9,6 +9,7 @@ import math
 
 import pygame, sys
 from pygame.locals import *
+from button import Button
 
 
 c = 0.5*(10)
@@ -585,5 +586,189 @@ while run:
     draw_text(str(mainClock.get_fps()), pygame.font.SysFont("comicsansms", 100), (255, 0, 0), window, 0, 0)
         
     mainClock.tick(60)
+
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("assets/wp7872665.png")
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+    
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        
+        SCREEN.blit(BG,(0,0))
+
+        OPTIONS_TEXT = get_font(45).render("Choose Difficulty", True, "White")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 100))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        EASY_BUTTON = Button(image=None, pos=(320, 250), 
+                            text_input="Easy", font=get_font(50), base_color="White", hovering_color="#d7fcd4")
+        NORMAL_BUTTON = Button(image=None, pos=(640, 250), 
+                            text_input="Normal", font=get_font(50), base_color="White", hovering_color="#d7fcd4")
+        HARD_BUTTON = Button(image=None, pos=(960, 250), 
+                            text_input="Hard", font=get_font(50), base_color="White", hovering_color="#d7fcd4")
+
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 400), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="#d7fcd4")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        EASY_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        NORMAL_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        HARD_BUTTON.changeColor(OPTIONS_MOUSE_POS)
+        EASY_BUTTON.update(SCREEN)
+        NORMAL_BUTTON.update(SCREEN)
+        HARD_BUTTON.update(SCREEN)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if EASY_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    # Set the difficulty level to easy
+                    print("Easy difficulty selected")
+                if NORMAL_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    # Set the difficulty level to normal
+                    print("Normal difficulty selected")
+                if HARD_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    # Set the difficulty level to hard
+                    print("Hard difficulty selected")
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(90).render("Galaxy Quest", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Novi_play_backround.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="White", hovering_color="#d7fcd4")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Button_Difficulty_Backround.png"), pos=(640, 400), 
+                            text_input="DIFFICULTY", font=get_font(75), base_color="White", hovering_color="#d7fcd4")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Novi_quit_backround.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="White", hovering_color="#d7fcd4")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+main_menu()
+
+def game_over():
+    while True:
+        GAME_OVER_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        GAME_OVER_TEXT = get_font(100).render("GAME OVER", True, "Red")
+        GAME_OVER_RECT = GAME_OVER_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(GAME_OVER_TEXT, GAME_OVER_RECT)
+
+        GAME_OVER_RETRY = Button(image=None, pos=(640, 460),
+                                text_input="RETRY", font=get_font(75), base_color="White", hovering_color="Green")
+
+        GAME_OVER_RETRY.changeColor(GAME_OVER_MOUSE_POS)
+        GAME_OVER_RETRY.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if GAME_OVER_RETRY.checkForInput(GAME_OVER_MOUSE_POS):
+                    play()
+
+        pygame.display.update()
+
+def pause():
+    while True:
+        PAUSE_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PAUSE_TEXT = get_font(100).render("PAUSED", True, "White")
+        PAUSE_RECT = PAUSE_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PAUSE_TEXT, PAUSE_RECT)
+
+        PAUSE_RESUME = Button(image=None, pos=(640, 460),
+                                text_input="RESUME", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PAUSE_QUIT = Button(image=None, pos=(640, 560),
+                                text_input="QUIT", font=get_font(75), base_color="White", hovering_color="Red")
+
+        PAUSE_RESUME.changeColor(PAUSE_MOUSE_POS)
+        PAUSE_QUIT.changeColor(PAUSE_MOUSE_POS)
+        PAUSE_RESUME.update(SCREEN)
+        PAUSE_QUIT.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PAUSE_RESUME.checkForInput(PAUSE_MOUSE_POS):
+                    return
+                if PAUSE_QUIT.checkForInput(PAUSE_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
     
 
